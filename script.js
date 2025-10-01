ef4601cf7ecf28966a5b32693c7dc2cce55f4ac1
Klayver Alencar <alencar.klayver7@gmail.com>
2025-10-01 22:13:58 +0000

    primeira visualização

---
+++ script.js
@@ -0,0 +1,5 @@
+// Exemplo simples: botão para mostrar alert com promoções
+document.getElementById('btnPromo').addEventListener('click', () => {
+  alert('Confira nossas promoções especiais! Descontos de até 30% em guitarras e baterias!');
+});
+
